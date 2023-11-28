7fcc2f05e83f46536d74c8c6630f0765319ec502
Harry Findlay <harryfindlay@outlook.com>
2023-11-28 16:06:39 +0000

    init DBSCAN

--- disasters_visualisation.py
+++ disasters_visualisation.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
 
 from helper_functions import distance_between_points
 
@@ -12,10 +14,22 @@ disasters_df = disasters_df.copy()
 disasters_df['Year'] = disasters_df['DisNo.']
 disasters_df['Year'] = disasters_df['Year'].apply(lambda x: int((str(x).split("-"))[0]))
 
-years = [x for x in range(disasters_df['Year'].min(), disasters_df['Year'].max() + 1)]
+# drop rows with nan lat long coords
+disasters_df.dropna(how='any', subset=['Latitude', 'Longitude'], inplace=True)
 
-for col in disasters_df.columns:
-    print(col)
+points = disasters_df[['Latitude', 'Longitude']].to_numpy()
+
+clustering = DBSCAN(eps=3, min_samples=2, metric=distance_between_points).fit(points)
+num_clusters = len(set(clustering.labels_))
+clusters = pd.Series(points[clustering.labels_ == n] for n in range(num_clusters))
+
+print(clusters.head())
+
+print(f'Number of Clusters: {num_clusters}')
+
+
+# sum of deaths per year
+years = [x for x in range(disasters_df['Year'].min(), disasters_df['Year'].max() + 1)]
 
 average_deaths_df = pd.DataFrame({'Year': years})
 average_deaths_df['CumDeaths'] = average_deaths_df['Year']
@@ -26,11 +40,23 @@ average_deaths_df['CumDeaths'] = average_deaths_df['CumDeaths'].map(disasters_df
 # point_b = (17.5, -102.5)
 # print(distance_between_points(point_a, point_b))
 
-plt.figure(figsize=(12, 7))
-plt.plot(average_deaths_df['Year'], average_deaths_df['CumDeaths'], 'b-', label='Total deaths')
-plt.xlabel('Year')
-plt.ylabel('Deaths')
-plt.title('Average Deaths per Year from Disasters')
-plt.legend()
-plt.grid(True)
+# plt.figure(figsize=(12, 7))
+# plt.plot(average_deaths_df['Year'], average_deaths_df['CumDeaths'], 'b-', label='Total deaths')
+# plt.xlabel('Year')
+# plt.ylabel('Deaths')
+# plt.title('Average Deaths per Year from Disasters')
+# plt.legend()
+# plt.grid(True)
+
+# plt.figure(figsize=(10, 6))
+# plt.scatter(disasters_df['Longitude'], disasters_df['Latitude'], label="Locations of disasters")
+# plt.xlabel('Longitude')
+# plt.ylabel('Latitude')
+# plt.title('Lat Long coordiantes of Disasters from 1900')
+# plt.legend()
+# plt.grid(True)
+
+# visualising clusters
+color_list = np.array(['green', 'blue', 'red', 'yellow',  'orange',  'magenta', 'cyan', 'purple'])
+
 plt.show()
